7f93abe26fb7f2fce0ff45a89193900645edbbb7
GuidoDipietro <dipietroguido@gmail.com>
2020-05-30 17:13:35 -0300

    More functionalities to worst_of_X.py

--- FMCavg100/worst_of_X.py
+++ FMCavg100/worst_of_X.py
@@ -13,22 +13,35 @@ latest = 400
 data = data[-1*latest:]
 data = np.array([float(x) for x in data])
 
-# Chunk size
-chunk = 50
+# chunk size
+chunk_size = 50
+step_size = 25
 
-a = []
-for index in reversed(np.arange(len(data)-chunk,0,-1*chunk)):
-	a.append(max(data[index:index+chunk]))
+######## Partition approach ########
+
+a = [[],[],[]]
+for index in reversed(np.arange(len(data)-chunk_size,0,-1*chunk_size)):
+	chunk = data[index:index+chunk_size]
+	a[0].append(max(chunk))
+	a[1].append(np.unique(chunk)[-2])
+	a[2].append(np.unique(chunk)[-3])
 a = np.array(a)
-print("Partition of latest {} into chunks of {}.".format(latest, chunk))
-print("Worst by chunk: ", a)
-print("Mean of all: {}".format(a.mean()))
+print("Partition of latest {} into chunks of {}.".format(latest, chunk_size))
+print("Worst by chunk: {}, mean = {}".format(a[0], a[0].mean()))
+print("2nd worst by chunk: {}, mean = {}".format(a[1], a[1].mean()))
+print("3rd worst by chunk: {}, mean = {}".format(a[2], a[2].mean()))
 print("")
 
-b = []
-for index in reversed(np.arange(len(data)-chunk,0,-1*(chunk//2))):
-	b.append(max(data[index:index+chunk]))
+######## Scan approach ########
+
+b = [[],[],[]]
+for index in reversed(np.arange(len(data)-chunk_size,0,-1*step_size)):
+	chunk = data[index:index+chunk_size]
+	b[0].append(max(chunk))
+	b[1].append(np.unique(chunk)[-2])
+	b[2].append(np.unique(chunk)[-3])
 b = np.array(b)
-print("Scan of latest {} into chunks of {}, step size = {}.".format(latest, chunk, chunk//2))
-print("Worst by chunk ", b)
-print("Mean of all: {}".format(b.mean()))
+print("Scan of latest {} into chunks of {}, step size = {}.".format(latest, chunk_size, step_size))
+print("Worst by chunk: {}, mean = {}".format(b[0], b[0].mean()))
+print("2nd worst by chunk: {}, mean = {}".format(b[1], b[1].mean()))
+print("3rd worst by chunk: {}, mean = {}".format(b[2], b[2].mean()))
